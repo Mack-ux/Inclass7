bbcc43d5cec85fb6a9f042ff019487f47365c6d2
MacauleyAmobi <152371682+Macauley202@users.noreply.github.com>
2025-02-26 13:53:03 -0500

    Adding circular edges that toggle on or off

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,65 @@
 import 'package:flutter/material.dart';
+
 void main() {
-runApp(MyApp());
+  runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-home: FadingTextAnimation(),
-);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ImageWithFrameToggle(),
+    );
+  }
 }
+
+class ImageWithFrameToggle extends StatefulWidget {
+  @override
+  _ImageWithFrameToggleState createState() => _ImageWithFrameToggleState();
 }
-class FadingTextAnimation extends StatefulWidget {
-@override
-_FadingTextAnimationState createState() => _FadingTextAnimationState();
-}
-class _FadingTextAnimationState extends State<FadingTextAnimation> {
-bool _isVisible = true;
-void toggleVisibility() {
-setState(() {
-_isVisible = !_isVisible;
-});
-}
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-appBar: AppBar(
-title: Text('Fading Text Animation'),
-),
-body: Center(
-child: AnimatedOpacity(
-opacity: _isVisible ? 1.0 : 0.0,
-duration: Duration(seconds: 4),
-child: Text(
-'Hello, Flutter!. This is a test',
-style: TextStyle(fontSize: 24),
-),
-),
-),
-floatingActionButton: FloatingActionButton(
-onPressed: toggleVisibility,
-child: Icon(Icons.play_arrow),
-),
-);
-}
+
+class _ImageWithFrameToggleState extends State<ImageWithFrameToggle> {
+  bool _isFrameVisible = false;  // Track whether the frame is visible
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image with Rounded Corners & Frame Toggle'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16), // Rounded corners
+              child: Container(
+                decoration: BoxDecoration(
+                  border: _isFrameVisible
+                      ? Border.all(color: Colors.blue, width: 4) // Blue border
+                      : null,
+                ),
+                child: Image.asset(
+                  'assets/image.png', // Replace with your image path
+                  width: 200, // You can adjust the size
+                  height: 200,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          SwitchListTile(
+            title: Text('Toggle Frame'),
+            value: _isFrameVisible,
+            onChanged: (bool value) {
+              setState(() {
+                _isFrameVisible = value;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
 }
